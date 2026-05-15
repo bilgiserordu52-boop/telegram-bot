076b39f0cab9ac8fbb25bbf035e346adb4ffd78f
Arda <ardansks1457@gmail.com>
2026-05-15 22:31:34 +0300

    auto deploy Fri May 15 22:31:34 +03 2026

--- bot.py
+++ bot.py
@@ -1,25 +1,25 @@
-import os
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters
 
 from commands.start import start_cmd
-from commands.admin import admin_cmd
+from commands.admin import admin_cmd, rollback_cmd
 from commands.message import handle_message
 from ui.admin_panel import button_handler
-from commands.admin import rollback_cmd
-
 import config
+
 
 def main():
     app = ApplicationBuilder().token(config.TOKEN).build()
 
     app.add_handler(CommandHandler("start", start_cmd))
-    app.add_handler(CommandHandler("rollback", rollback_cmd))
     app.add_handler(CommandHandler("admin", admin_cmd))
+    app.add_handler(CommandHandler("rollback", rollback_cmd))
+
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.add_handler(CallbackQueryHandler(button_handler))
 
-    print("BOT RUNNING (MODULAR)")
+    print("BOT RUNNING (V2 CLEAN ARCHITECTURE)")
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
